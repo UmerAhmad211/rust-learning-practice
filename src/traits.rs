7c4ed9456d82d6289044e29b1e2df4d55ac5e5ea
Umer Ahmad Khan <149548566+UmerAhmad211@users.noreply.github.com>
2024-07-23 23:11:49 +0500

    Add files via upload

---
+++ src/traits.rs
@@ -0,0 +1,44 @@
+pub trait summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("Read more from .....{}", self.summarize_author())
+    }
+}
+struct news_article {
+    headline: String,
+    location: String,
+    author: String,
+    content: String,
+}
+
+impl summary for news_article {
+    fn summarize_author(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+struct tweet {
+    username: String,
+    content: String,
+    reply: bool,
+    retweet: bool,
+}
+
+impl summary for tweet {
+    // fn summarize(&self) -> String {
+    //   format!("{}: {}", self.username, self.content)
+    // }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+fn main() {
+    let t1 = tweet {
+        username: String::from("Neil Halstead"),
+        content: String::from("allison im lost"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", t1.summarize());
+}
